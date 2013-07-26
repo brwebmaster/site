f9f2797bb6bba7c33a47a37e46a1b401da606811
Rahul Pandey <rpandey1234@gmail.com>
2013-07-25 22:29:12 -0700

    add user profile

--- db/migrate/20130628050947_create_users.rb
+++ db/migrate/20130628050947_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :bio
-
+      t.column :first_name,       :string
+      t.column :last_name,       :string
+      t.column :year,       :string
+      t.column :bio,       	:text
       t.timestamps
     end
   end
---
+++ db/migrate/20130726042216_load_data.rb
@@ -0,0 +1,12 @@
+class LoadData < ActiveRecord::Migration
+  def up
+  	u1 = User.new(:first_name => "Rahul", :last_name => "Pandey", :year => "2013", :bio => "I like to dance and smile and listen!")
+  	u1.save(:validate => false)
+  	u2 = User.new(:first_name => "Tulsee", :last_name => "Doshi", :year => "2015", :bio =>"Dancing and singing all the way")
+  	u2.save(:validate => false)
+  end
+
+  def down
+  	User.delete_all
+  end
+end
