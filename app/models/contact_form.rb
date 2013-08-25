27de4fc0b9de9d4b69d3884b8a00c7eb41ced772
Tulsee Doshi <tulsee27@gmail.com>
2013-08-25 12:54:01 -0700

    contact form functionality done

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -2,6 +2,7 @@ class ContactForm < MailForm::Base
 	attribute :name, :validate => true
 	attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   	attribute :file,      :attachment => true
+    attribute :inquiry
 
   	attribute :message
   	attribute :nickname,  :captcha  => true
