5f4bc1791015de59dce81adf39ef0ab00bb9b5c7
Tulsee Doshi <tulsee27@gmail.com>
2013-09-08 21:22:04 -0700

    performance page links fixed

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -6,10 +6,10 @@ def index
 	@performances = Performance
     .where("time > ?", Time.now)
     .order('time ASC')
-	@years = {"2012-2013" => {"videoID" => "xDUMIMu7Ojg", "awards" => ["2nd Place - ATown Showdown"], "videos" => ["http://www.youtube.com/watch?v=ypDAKsCmUcg", "http://www.youtube.com/watch?v=lpQPRE-RfA8 "]}, 
+	@years = {"2012-2013" => {"videoID" => "xDUMIMu7Ojg", "awards" => ["2nd Place - ATown Showdown"]}, 
 			"2011-2012" => {"videoID" => "fA0PSrRdbPk", "awards" => ["1st Place - Raas Rodeo", "3rd Place - Garba with Attitude", "Invitation to Raas All-Stars"]}, 
 			"2010-2011" => {"videoID" => "dJV0-jp77P0", "awards" => ["3rd Place - Garba with Attitude"]},
-			"2009-2010" => {"videoID" => false, "awards" => ["1st Place - Garbafest", "3rd Place = Garba with Attitude", "Invitation to Raas All-Stars"], "photo" => "20092010.jpg"},
+			"2009-2010" => {"videoID" => false, "awards" => ["1st Place - Garbafest", "3rd Place - Garba with Attitude", "Invitation to Raas All-Stars"], "photo" => "20092010.jpg"},
 			"2008-2009" => {"videoID" => "4D4YxO7sumI", "awards" => ["3rd Place - Rangeelo Raas"]},
 			"2007-2008" => {"videoID" => false, "awards" => ["1st Place - Garba with Attitude", "2nd Place - Rangeelo Raas", "Invitation to Best of the Best 4"],"videos" => ["http://youtube.com/watch?v=C5lE62C_BCo"], "photo" => "2007-2008.jpg"},
 			"2006-2007" => {"videoID" => false, "awards" => ["1st Place - Rangeelo Raas", "2nd Place - Garden State Garba", "Hype Award - Rangeelo Raas"], "videos" => ["http://youtube.com/watch?v=IwT96UxCpJY"], "photo" => "2006-2007.jpg"},
