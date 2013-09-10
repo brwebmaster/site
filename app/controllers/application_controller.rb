195a33f5825cb02ffa3f6bbdc125f1e700724ef9
Rahul Pandey <rpandey1234@gmail.com>
2013-09-10 00:56:48 -0400

    get rid of unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,30 +16,4 @@ class ApplicationController < ActionController::Base
 	def server_error
 	  raise Exception
 	end
-
-	if Rails.env.production?
-    unless Rails.application.config.consider_all_requests_local
-      rescue_from Exception, with: :render_500
-      rescue_from ActionController::RoutingError, with: :render_404
-      rescue_from ActionController::UnknownController, with: :render_404
-      rescue_from ActionController::UnknownAction, with: :render_404
-      rescue_from ActiveRecord::RecordNotFound, with: :render_404
-    end
-  end
-
-  def render_404(exception)
-    @not_found_path = exception.message
-    respond_to do |format|
-      format.html { render template: 'errors/not_found', layout: 'layouts/application', status: 404 }
-      format.all { render nothing: true, status: 404 }
-    end
-  end
- 
-  def render_500(exception)
-    respond_to do |format|
-      format.html { render template: 'errors/internal_server_error', layout: 'layouts/application', status: 500 }
-      format.all { render nothing: true, status: 500}
-    end
-  end
-
 end
