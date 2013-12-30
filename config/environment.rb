be5d913e949b039187ad5e1002b41a57cc5261d9
Rahul Pandey <rpandey1234@gmail.com>
2013-12-29 23:40:22 -0500

    use foreman to manage facebook dev credentials

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
-
-FACEBOOK_APP_ID = FACEBOOK_CONFIG['app_id']
-
 # Initialize the rails application
 Brsite::Application.initialize!
