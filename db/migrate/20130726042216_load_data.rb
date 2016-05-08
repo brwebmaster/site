95e2438d57105d0ed7663df60ee434b575ae6df3
Rahul Pandey <rahul@pinterest.com>
2016-05-07 22:12:26 -0700

    update load data migration so is_alumni is set

--- db/migrate/20130726042216_load_data.rb
+++ db/migrate/20130726042216_load_data.rb
@@ -1,8 +1,8 @@
 class LoadData < ActiveRecord::Migration
   def up
-  	u1 = User.new(:first_name => "Rahul", :last_name => "Pandey", :year => "2013", :bio => "I like to dance and smile and listen!")
+  	u1 = User.new(:first_name => "Rahul", :last_name => "Pandey", :year => "2013", :bio => "I like to dance and smile and listen!", :is_alumni => true)
   	u1.save(:validate => false)
-  	u2 = User.new(:first_name => "Tulsee", :last_name => "Doshi", :year => "2015", :bio =>"Dancing and singing all the way")
+  	u2 = User.new(:first_name => "Tulsee", :last_name => "Doshi", :year => "2015", :bio =>"Dancing and singing all the way", :is_alumni => false)
   	u2.save(:validate => false)
   end
 
